cd9cfb0159cf2b631f39a28d762bf1172fa5db65
kubilaycaglayan <kblycaglayan@gmail.com>
2020-08-14 23:10:38 +0300

    Add music

--- src/Config/config.js
+++ src/Config/config.js
@@ -5,4 +5,11 @@ export default {
   parent: 'phaser-example',
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
 };
--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -85,7 +85,8 @@ export default class PreloaderScene extends Phaser.Scene {
     this.load.image('phaserLogo', 'assets/logo.png');
     this.load.image('box', 'assets/ui/grey_box.png');
     this.load.image('checkedBox', 'assets/ui/blue_boxCheckmark.png');
-    this.load.audio('bgMusic', ['assets/TownTheme.mp3']);
+    this.load.audio('bgMusic', ['assets/Srydorm.mp3']);
+    this.load.audio('starMusic', ['assets/star.mp3']);
   }
 
   ready() {
