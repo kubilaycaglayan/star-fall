0a2588cc162424568d48573258d3de3d3f46a69f
kubilaycaglayan <kblycaglayan@gmail.com>
2020-08-14 22:44:10 +0300

    Add animations into GameScene

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -91,5 +91,25 @@ export default class GameScene extends Phaser.Scene {
       this.stMusic.play();
     };
     this.physics.add.overlap(this.player, stars, this.collectStar, null, this);
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frames: [{ key: 'dude', frame: 4 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
   }
 }
